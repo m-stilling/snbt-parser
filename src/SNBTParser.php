366b0fa568adbd126d07bf737de4c037e2b1be3b
Stilling <git@stilling.dev>
2025-09-18 19:02:59 +0200

    refactor: recursive -> iterative parsing
    massive reductions in memory usage

--- src/SNBTParser.php
+++ src/SNBTParser.php
@@ -7,9 +7,14 @@ use Stilling\SNBTParser\Tokens\Token;
 
 class SNBTParser {
 	public static function parse(string $input): array|float|int|string|object|bool {
-		$tokens = static::readTokens(mb_trim($input));
+		$json = static::readSNBT(mb_trim($input));
+		$array = json_decode($json, true);
 
-		return static::jsonParseTokens($tokens);
+		if (json_last_error() !== JSON_ERROR_NONE) {
+			throw new \Exception("SNBT is malformed, failed to decode JSON: " . json_last_error_msg());
+		}
+
+		return $array;
 	}
 
 	public static function intsToUuid(array $ints): string {
@@ -36,56 +41,31 @@ class SNBTParser {
 	}
 
 	/**
-	 * @param string $token
-	 * @param Token[] $tokens
-	 * @return Token[]
+	 * @param string $snbt
+	 * @return string
 	 * @throws \Exception
 	 */
-	protected static function readTokens(string $token, array $tokens = []): array {
-		if (count($tokens) === 0) {
-			$currentToken = new InitialToken();
-		} else {
-			$currentToken = end($tokens);
-		}
-
-		[ $nextToken, $remaining ] = $currentToken->parseNextToken($token);
-
-		if (
-			!isset($nextToken)
-			|| !($nextToken instanceof Token)
-			|| !isset($remaining)
-			|| !is_string($remaining)
-		) {
-			throw new \Exception("Invalid parseNextToken result");
-		}
-
-		$tokens[] = $nextToken;
-
-		if (mb_strlen($remaining) > 0) {
-			return static::readTokens($remaining, $tokens);
-		}
-
-		return $tokens;
-	}
-
-	/**
-	 * @param Token[] $tokens
-	 * @return array|float|int|string|object|bool
-	 * @throws \Exception
-	 */
-	protected static function jsonParseTokens(array $tokens): array|float|int|string|object|bool {
+	protected static function readSNBT(string $snbt): string {
 		$json = "";
+		$currentToken = new InitialToken();
 
-		foreach ($tokens as $token) {
-			$json .= $token->toJsonToken();
+		while (mb_strlen($snbt) > 0) {
+			[ $nextToken, $remainingSNBT ] = $currentToken->parseNextToken($snbt);
+
+			if (
+				!isset($nextToken)
+				|| !($nextToken instanceof Token)
+				|| !isset($remainingSNBT)
+				|| !is_string($remainingSNBT)
+			) {
+				throw new \Exception("Invalid parseNextToken result");
+			}
+
+			$json .= $nextToken->toJsonToken();
+			$snbt = $remainingSNBT;
+			$currentToken = $nextToken;
 		}
 
-		$array = json_decode($json, true);
-
-		if (json_last_error() !== JSON_ERROR_NONE) {
-			throw new \Exception("SNBT is malformed, failed to decode JSON: " . json_last_error_msg());
-		}
-
-		return $array;
+		return $json;
 	}
 }
